8ed9f48550d5def5a2b239ea620dd0b5f6f5ba02
AkmalFadlurohman <fadlurohmanakmal@rocketmail.com>
2018-11-12 02:50:20 +0700

    Migrate to zend-soap

--- task-services/SOAP/SoapClient.php
+++ task-services/SOAP/SoapClient.php
@@ -4,10 +4,8 @@ $options= array(
   'location' 	=>	'http://localhost:8080/SoapServer.php',
   'uri'		=>	'http://localhost:8080/test'
 );
-
 $client=new SoapClient(NULL,$options);
-//echo $client->getMessage();  //Hello,World!
-//echo $client->addNumbers(3,5);
-echo $client->__soapCall('addNumbers', array(3, 5));
+echo $client->getMessage();  //Hello,World!
+echo $client->addNumbers(3,5);
 
 ?>
